d016f713452840991d3d03356701ed718d655fe3
misha <misha.pavlovich.2001@mail.ru>
2020-12-22 20:29:35 +0300

    commit_1,2

--- Project/views.py
+++ Project/views.py
@@ -6,17 +6,21 @@ from bs4 import BeautifulSoup
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(['Post'])
 def time_manager(request):
+    request = json.loads(request)
     response = {
-        'session': request.json['session'],
-        'version': request.json['version'],
+        'session': request['session'],
+        'version': request['version'],
         'response': {
             'end_session': True
         }
     }
     response['response']['text'] = '40'
+    get_time('brest', 'autobus', '40', "Гвардейская%20-%20Газоаппарат", 'Гвардейская', response, request.json, )
     return json.dumps(response)
+
 
 def get_time(city, type, number, direction, stop):
     url = f'https://kogda.by/routes/{city}/{type}/{number}/{direction}/{stop}'
@@ -27,6 +31,3 @@ def get_time(city, type, number, direction, stop):
     soup = BeautifulSoup(response.text, 'lxml')
     times = soup.find_all('span', class_='future')
     return list(map(lambda x: x.text.strip().replace(':', ' '), times))
-
-
-
